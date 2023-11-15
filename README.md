d14d702230920cb54acdcfce6e95cf33e675a096
Franco Du Plessis <67424625+Franky-Dee@users.noreply.github.com>
2023-11-15 12:32:39 +0200

    Creation of simple web app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KeyCode Mini Project
  This is a small web application with the purpose of practicing the use of javascript
+
+## Using this web app
+This is a very simple web app that requires the user to press any key on their keyboard, when the user does so they will be provided with the key that was pressed, the key code that is associated with that key and the code associated with that key.
---
+++ script.js
@@ -0,0 +1,20 @@
+window.addEventListener('keydown', (e) => {
+    const insert = document.getElementById('insert');
+
+    insert.innerHTML = `
+    <div class="key">
+        ${e.key}
+        <small>Key</small>
+      </div>
+
+      <div class="key">
+        ${e.keyCode}
+        <small>KeyCode</small>
+      </div>
+
+      <div class="key">
+        ${e.code}
+        <small>Code</small>
+      </div>
+    `;
+});
